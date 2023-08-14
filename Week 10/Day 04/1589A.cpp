319f9670abb55f465ac585937a53c5ccc5eba455
worshipper <shakibprof2001@gmail.com>
2023-08-14 23:36:58 +0600

    test

---
+++ Week 10/Day 04/1589A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long x, y;
+        cin >> x >> y;
+        cout << -x * x << " " << y * y << endl;
+    }
+}
